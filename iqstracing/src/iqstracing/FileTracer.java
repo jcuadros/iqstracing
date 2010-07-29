43a8e5e286addd418cff4daacd53e5b516798ce9
nuriacoronas <nuriacoronas@gmail.com>
2010-07-29 18:27:31 +0000

    The tracer classes now write the information that will be traced to a file or to the console.

--- iqstracing/src/iqstracing/FileTracer.java
+++ iqstracing/src/iqstracing/FileTracer.java
@@ -7,18 +7,44 @@ public class FileTracer extends Tracer {
 
 	public FileTracer (String user, String session, String file){
 		super(user,session);
-		this.file = new File(file);
+		if (file.endsWith(".xml"))
+			this.file = new File(file);
+		else
+			this.file = new File(file + ".xml");
 	}
 	public FileTracer (String user, String session){
 		super(user,session);
-		this.file = new File(session);
+		this.file = new File(session + ".xml");
 	}
 
-	public void trace(Event event, String time, double time_ms, int sequence){
-
+	public void trace(Event event, String time, long time_ms, int sequence){
+		try {
+			StringBuilder sb = new StringBuilder();
+			sb.append("<event application=\"" + this.application + "\" ");
+			sb.append("action=\"" + event.actionName + "\" ");
+			sb.append("user=\"" + this.user + "\" ");
+			sb.append("session=\"" + session + "\" ");
+			sb.append("time=\"" + time + "\" ");
+			sb.append("session=\"" + this.session + "\" ");
+			sb.append("time_ms=\"" + time_ms + "\" ");
+			sb.append("number=\"" + sequence + "\" ");
+			sb.append("type=\"" + event.eventType + "\" ");
+			sb.append("action_ref=\"" + event.eventPreReference + "\"");
+			sb.append(">\n");
+			if (event.actionParameters != null){
+				for (String key : event.actionParameters.keySet()) {
+					sb.append("\t<param name=\"" + key + "\" value=\"" + event.actionParameters.get(key)
+								+ "\"/>\n");
+				}
+			}
+			sb.append("</event>");
+			FileWriter writer = new FileWriter(file);
+			writer.write(sb.toString());
+			writer.close();
+		} catch (IOException ignored) {}
 	}
 
-	public void trace(State state, String time, double time_ms, int sequence){
+	public void trace(State state, String time, long time_ms, int sequence){
 
 	}
 }
