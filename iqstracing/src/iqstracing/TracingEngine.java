dca2c76f2f11618abdb9ced815fb1bc59bda9c30
nuriacoronas <nuriacoronas@gmail.com>
2010-07-29 17:04:20 +0000

    Constructors have been modified. When a TracingEngine object is created, values are assigned to the fields time and time_ms.

--- iqstracing/src/iqstracing/TracingEngine.java
+++ iqstracing/src/iqstracing/TracingEngine.java
@@ -1,6 +1,8 @@
 package iqstracing;
 
+import java.text.SimpleDateFormat;
 import java.util.*;
+import java.io.*;
 
 public class TracingEngine {
 	//Fields:
@@ -9,46 +11,66 @@ public class TracingEngine {
 	int sequence;
 	String session;
 	String time;
-	String time_ms;
+	long time_ms;
 	Collection<Tracer> tracerCollection;
-	boolean status;
+	boolean status = true;
 
 	//Constructors:
 	public TracingEngine (String application, String user, String session){
 		this.application = application;
 		this.user = user;
 		this.session = session;
+		this.time = (new SimpleDateFormat("yyyyMMddHHmmss zzz")).format(new Date());
+		this.time_ms = System.currentTimeMillis();
 	}
+
 	public TracingEngine (String application, String user){
 		this.application = application;
 		this.user = user;
 		this.session = user + "_" + RandomGenerator.generate(6);
+		this.time = (new SimpleDateFormat("yyyyMMddHHmmss zzz")).format(new Date());
+		this.time_ms = System.currentTimeMillis();
 	}
 	public TracingEngine (String application){
 		this.application = application;
 		this.user = RandomGenerator.generate(6);
 		this.session = this.user + "_" + RandomGenerator.generate(6);
+		this.time = (new SimpleDateFormat("yyyyMMddHHmmss zzz")).format(new Date());
+		this.time_ms = System.nanoTime()/1000000;
 	}
 
 	//Methods:
-	public void setTracingStatus(boolean newStatus){
-		status=newStatus;
+	public void setStatus(boolean b){
+		this.status = b;
+	}
+	public boolean getStatus(){
+		return this.status;
 	}
 
-	public boolean getTracingStatus(){
-		return status;
+	public void startTracingToFile(File file){
+
 	}
 
-	/*public void startTracingToFile(){
+	public void stopTracingToFile(File file){
+
 	}
-	public void stopTracingToFile(){
-	}
+
 	public void startTracingToConsole(){
+
 	}
+
 	public void stopTracingToConsole(){
+
 	}
+
 	public void trace (Event event){
+
 	}
 	public void trace (State state){
-	}*/
+
+	}
+
+	public static void main (String[]args){
+
+	}
 }
