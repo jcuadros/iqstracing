7094e974119c14522222dd9b1fa957001198782d
nuriacoronas <nuriacoronas@gmail.com>
2010-07-30 09:33:09 +0000

    PostTracer class created

---
+++ iqstracing/src/iqstracing/PostTracer.java
@@ -0,0 +1,22 @@
+package iqstracing;
+
+public class PostTracer extends Tracer {
+
+	public PostTracer(String User, String Session) {
+		super(User, Session);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void trace(Event event, String time, double time_ms, int sequence) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void trace(State state, String time, double time_ms, int sequence) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
