5e6eb14c81bf3d41fa854b7243abf7c92a1563a8
jordi.at.iqs <jordi.at.iqs@d285514b-14fe-9ef2-4fe1-4451f37a3a56>
2010-08-23 13:27:47 +0000

    Removed static modifiers from fields in TracingEngine. Added minimal test for tracing to file.

--- trunk/iqstracing/src/testsToRemove/JCTracingTest.java
+++ trunk/iqstracing/src/testsToRemove/JCTracingTest.java
@@ -1,5 +1,6 @@
 package testsToRemove;
 
+import java.io.File;
 import java.util.Hashtable;
 
 import iqstracing.Event;
@@ -12,6 +13,8 @@ public class JCTracingTest {
 		minimalTest();
 		fullySpecifiedMinimalTest();
 		oneEventWithParametersTest();
+		minimalTestToFile();
+
 	}
 
 	private static void minimalTest() {
@@ -55,7 +58,7 @@ public class JCTracingTest {
 	private static void oneEventWithParametersTest() {
 		//Create a tracing engine, which should trace by default (tracingStatus is true) to console
 		//User and session should be randomly generated
-		TracingEngine te=new TracingEngine("minimalTest");
+		TracingEngine te=new TracingEngine("oneEventWithParametersTest");
 
 		//Trace an event with parameters
 		Hashtable<String, String> parameters=new Hashtable<String,String>();
@@ -68,7 +71,7 @@ public class JCTracingTest {
 
 		/* EXPECTED
 		 * To console:
-		 * <event application="minimalTest" action="START" user="random" session="random"
+		 * <event application="oneEventWithParametersTest" action="START" user="random" session="random"
 		 * time="20010101000000 UTM" time_ms="1234567" number="1"><param name="par1" value="1"/>
 		 * <param name="par2" value="2"/><param name="par3" value="3"/></event>
 		 *
@@ -76,4 +79,27 @@ public class JCTracingTest {
 		 */
 	}
 
+	private static void minimalTestToFile() {
+		//Create a tracing engine, which should trace by default (tracingStatus is true) to console
+		//User and session should be randomly generated
+		TracingEngine te=new TracingEngine("minimalTestToFile");
+		te.stopTracingToConsole();
+
+		//Clear logtest1.log file and set tracing to it
+		new File("logtest1.log").delete();
+		te.startTracingToFile("logtest1.log");
+
+		//Trace a minimal event
+		Event e1=new Event("START", null, null, null, null);
+		te.trace(e1);
+
+		/* EXPECTED
+		 * To file "logtest1.log":
+		 * <event application="minimalTestToFile" action="START" user="random" session="random"
+		 * time="20010101000000 UTM" time_ms="1234567" number="1"></event>
+		 *
+		 * Nothing should be traced to console.
+		 * New lines are for readability only.
+		 */
+	}
 }
