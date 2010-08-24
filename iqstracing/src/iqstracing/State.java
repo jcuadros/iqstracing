7c62c77fcbd0647853256b214fe4227bc874593a
nuriacoronas <nuriacoronas@gmail.com>
2010-08-24 18:09:42 +0000

    Doc comments have been added to the classes State and Event.

--- iqstracing/src/iqstracing/State.java
+++ iqstracing/src/iqstracing/State.java
@@ -1,8 +1,22 @@
 package iqstracing;
 
+/**
+ * State is the class that contains the information about
+ * traces based on states instead of actions. Through a
+ * short description, it provides information about such
+ * state.
+ */
 public class State {
 	String description;
 
+	/**
+	 * Constructs a State object from a specified description.
+	 * This object will then be passed to a tracer.
+	 *
+	 * @param description String representing a human-readable
+	 * version of the state of the application which is being
+	 * traced
+	 */
 	public State(String description){
 		this.description = description;
 	}
