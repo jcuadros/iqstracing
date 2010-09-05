d7f9c403c49a051dac2386de616e5e53706102ae
nuriacoronas@gmail.com <nuriacoronas@gmail.com@d285514b-14fe-9ef2-4fe1-4451f37a3a56>
2010-09-05 22:13:39 +0000

    New tests created.

--- trunk/iqstracing/src/tests/JCTracingTest.java
+++ trunk/iqstracing/src/tests/JCTracingTest.java
@@ -12,19 +12,22 @@ public class JCTracingTest {
 
 	public static void main(String[] args) {
 		//Here the tests will be called
-		minimalTest();
-		minimalStateTest();
-		fullySpecifiedMinimalTest();
-		oneEventWithParametersTest();
-		oneEventWithParametersAndDescriptionTest();
-		oneEventWithDescriptionTest();
-		minimalTestToFile();
-		multipleEventTest();
-		multipleEventAndTracerTest();
-		openAndCloseTracersTest();
-		simultaneousTracingEnginesTest();
-		simultaneousTracingEnginesTest2();
-		deleteAndCreateFileTest();
+		//minimalTest();
+		//minimalStateTest();
+		//fullySpecifiedMinimalTest();
+		//oneEventWithParametersTest();
+		//oneEventWithParametersAndDescriptionTest();
+		//oneEventWithDescriptionTest();
+		//minimalTestToFile();
+		//multipleEventTest();
+		//multipleEventAndTracerTest();
+		//openAndCloseTracersTest();
+		//simultaneousTracingEnginesTest();
+		//simultaneousTracingEnginesTest2();
+		//deleteAndCreateFileTest();
+		//nullApplicationTest();
+		//nullActionNameTest();
+		repeatedFileNamesTest();
 	}
 
 	private static void minimalTest() {
@@ -138,8 +141,7 @@ public class JCTracingTest {
 
 		String description = "A brief description.";
 
-		Event e1 = new Event("START", parameters, null, null,
-								description);
+		Event e1 = new Event("START", parameters, description);
 		te.trace(e1);
 
 		/* EXPECTED
@@ -166,8 +168,8 @@ public class JCTracingTest {
 		//Trace an event with description and without parameters
 		String description = "A brief description.";
 
-		Event e1 = new Event("START", null, null, null,
-								description);
+		Event e1 = new Event("START", null, description,
+								null, null);
 		te.trace(e1);
 
 		/* EXPECTED
@@ -193,7 +195,7 @@ public class JCTracingTest {
 		te.startTracingToFile("logtest1.log");
 
 		//Trace a minimal event
-		Event e1 = new Event("START", null, null, null, null);
+		Event e1 = new Event("START");
 		te.trace(e1);
 
 		/* EXPECTED
@@ -223,8 +225,7 @@ public class JCTracingTest {
 
 		String description = "A brief description.";
 
-		Event e1 = new Event("START", parameters1, null, null,
-								description);
+		Event e1 = new Event("START", parameters1, description);
 
 		Hashtable<String, String> parameters2 =
 					new Hashtable<String, String>();
@@ -234,8 +235,7 @@ public class JCTracingTest {
 		parameters2.put("par7", "7");
 		parameters2.put("par8", "8");
 
-		Event e2 = new Event("SECOND EVENT", parameters2, null,
-								null, null);
+		Event e2 = new Event("SECOND EVENT", parameters2);
 
 		te.trace(e1);
 		te.trace(e2);
@@ -272,6 +272,8 @@ public class JCTracingTest {
 
 		String file1 = "FileTest1.log";
 		String file2 = "FileTest2.log";
+		new File(file1).delete();
+		new File(file2).delete();
 		te.startTracingToFile(file1);
 		te.startTracingToFile(file2);
 
@@ -284,18 +286,16 @@ public class JCTracingTest {
 
 		String description = "A brief description.";
 
-		Event e1 = new Event("START", parameters1, null, null,
-								description);
+		Event e1 = new Event("START", parameters1, description);
 
 		Hashtable<String, String> parameters2 =
 					new Hashtable<String, String>();
 		parameters2.put("par4", "4");
 		parameters2.put("par5", "5");
 
-		Event e2 = new Event("SECOND EVENT", parameters2, null,
-								null, null);
+		Event e2 = new Event("SECOND EVENT", parameters2);
 
-		Event e3 = new Event("THIRD EVENT", null, null, null, null);
+		Event e3 = new Event("THIRD EVENT");
 
 		te.trace(e1);
 		te.trace(e2);
@@ -334,6 +334,8 @@ public class JCTracingTest {
 		//Tracers:
 		String file1 = "openAndCloseTracersTest1.log";
 		String file2 = "openAndCloseTracersTest2.log";
+		new File(file1).delete();
+		new File(file2).delete();
 
 		//Different events:
 		Hashtable<String, String> parameters1 =
@@ -344,26 +346,23 @@ public class JCTracingTest {
 
 		String description = "A brief description.";
 
-		Event e1 = new Event("START", parameters1, null, null,
-								description);
+		Event e1 = new Event("START", parameters1, description);
 
 		Hashtable<String, String> parameters2 =
 					new Hashtable<String, String>();
 		parameters2.put("par4", "4");
 		parameters2.put("par5", "5");
-		Event e2 = new Event("SECOND EVENT", parameters2, null,
-						null, null);
+		Event e2 = new Event("SECOND EVENT", parameters2);
 
-		Event e3 = new Event("THIRD EVENT", null, null, null, null);
+		Event e3 = new Event("THIRD EVENT");
 
 		String description4 = "A description";
-		Event e4 = new Event("FOURTH EVENT", null, null,
-				null, description4);
+		Event e4 = new Event("FOURTH EVENT", null, description4);
 
-		Event e5 = new Event("FIFTH EVENT", null, "active", null, null);
+		Event e5 = new Event("FIFTH EVENT", null, null, "active", null);
 
-		Event e6 = new Event("SIXTH EVENT", null, "reactive",
-				"e5", null);
+		Event e6 = new Event("SIXTH EVENT", null, null,
+				"reactive", "e5");
 
 		//Tracing:
 		te.startTracingToFile(file2);
@@ -556,14 +555,14 @@ public class JCTracingTest {
 
 		Event[] e1 = new Event[5];
 		for (int i = 0; i < 5; i++) {
-			e1[i] = new Event("EVENT " + Integer.toString(i+1),
-						null, null, null, null);
+			e1[i] = new Event("EVENT " + Integer.toString(i + 1));
 		}
 		Event[] e2 = new Event[2];
-		e2[0] = new Event("FIRST EVENT", null, null, null, null);
-		e2[1] = new Event("SECOND EVENT", null, null, null, null);
+		e2[0] = new Event("FIRST EVENT");
+		e2[1] = new Event("SECOND EVENT");
 
 		String file = "anotherSimultaneousTest.log";
+		new File(file).delete();
 		te1.startTracingToFile(file);
 		te2.startTracingToFile(file);
 		for (Event e : e1) {
@@ -623,12 +622,7 @@ public class JCTracingTest {
 		e2[1] = new Event("SECOND EVENT", null, null, null, null);
 
 		String fileName = "existentFile.log";
-		try {
-			FileWriter out = new FileWriter(fileName);
-			out.close();
-			File file = new File(fileName);
-			file.delete();
-		} catch (IOException ignored) {}
+		new File(fileName).delete();
 
 		te1.startTracingToFile(fileName);
 		te2.startTracingToFile(fileName);
@@ -671,4 +665,78 @@ public class JCTracingTest {
 		*/
 	}
 
+	private static void nullApplicationTest() {
+		//Create a tracing engine with a null application, which
+		//should not trace but show an error message.
+		TracingEngine te1 = new TracingEngine("");
+		TracingEngine te2 = new TracingEngine(null);
+
+		Event event = new Event("START");
+
+		te1.trace(event);
+		te2.trace(event);
+
+		/*
+		 * EXPECTED:
+		 *
+		 * To console
+		 * Application not specified. The information will
+		 * not be traced.
+		 * Application not specified. The information will
+		 * not be traced.
+		 */
+	}
+
+	private static void nullActionNameTest() {
+		//Create an event with a null action name and
+		//check the tracing engine does not trace in
+		//this case.
+		TracingEngine te = new TracingEngine("nullActionNameTest");
+		Event e = new Event("");
+		te.trace(e);
+
+		/*
+		 * EXPECTED:
+		 *
+		 * To console
+		 * Action name of the event not specified. Event could not
+		 * be traced.
+		 */
+	}
+
+	private static void repeatedFileNamesTest() {
+		// Create a tracing engine that traces by default
+		// (tracing status is true) to console and to a file.
+		// This file has three possible "names" but the tracing
+		// engine must detect they're the same file.
+		TracingEngine te = new TracingEngine("repeatedFileNamesTest");
+		String file1 = "repeatTest.log";
+		String file2 = "RepeatTest.log";
+		String file3 = "C:\\Documents and Settings\\jcoronas\\"
+				+ "Escritorio\\NURIA\\JAVA\\workspace\\"
+				+ "iqstracing\\repeatTest.log";
+
+		new File(file1).delete();
+		te.startTracingToFile(file1);
+		te.startTracingToFile(file2);
+		te.startTracingToFile(file3);
+
+		Event e1 = new Event("START");
+		Event e2 = new Event("STOP");
+
+		te.trace(e1);
+		te.stopTracingToConsole();
+		te.stopTracingToFile(file3);
+		te.trace(e2);
+
+		/*
+		 * EXPECTED:
+		 *
+		 * To console and file "repeatTest.log"
+		 * <event application="repeatedFileNamesTest"
+		 * action="START" user=random session=random
+		 * time="20100905214156 GMT" time_ms="1.8333931E7"
+		 * number="1"></event>
+		 */
+	}
 }
